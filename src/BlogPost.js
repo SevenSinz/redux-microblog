3fc2bed70701ebf56ce918578b60281f6cbbc8a2
Stéphane Dryefus <floatingrock@gmail.com>
2019-04-03 12:14:35 -0700

    Add BlogPost details.

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
 
-// Need: id, title, descriptio, body
 // Edit button and Delete button
 
 
 
 class BlogPost extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isEditing: false
+        }
+        this.toggleIsEditing = this.toggleIsEditing.bind(this);
+    }
     
     findBlog() {
         const blogId = this.props.match.params.id;
         return this.props.blogPosts.filter(post => post.id === blogId); 
     }
 
+    /** When clicked switches state isEditing between true and false.
+     * This will render either the blog post or the blog edit form. */
+    toggleIsEditing() {
+        this.setState(
+            { isEditing: this.state.isEditing ? false : true }
+        );
+    }
+
     render() {
         const blogPost = this.findBlog()[0]
-        console.log("What props?", this.props)
+        console.log("What props?", this.props);
+        console.log("What is state?", this.state);
         return (
             <div>
-                <h2> {blogPost.title} </h2>
-                <p> {blogPost.description} </p>
-                <p> {blogPost.body} </p>
+                <button onClick={this.toggleIsEditing}> Edit </button>
+                <button onClick={this.props.handleDelete}> Delete </button>
+                <div>
+                    <h2> {blogPost.title} </h2>
+                    <p> {blogPost.description} </p>
+                    <p> {blogPost.body} </p>
+                </div>
             </div>
         );
     }
