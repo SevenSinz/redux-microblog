a3040bf128f520e165608c3409d8af85d5da3b8c
Stéphane Dryefus <floatingrock@gmail.com>
2019-04-04 08:36:25 -0700

    Small convention edit.

--- src/BlogHome.js
+++ src/BlogHome.js
@@ -5,7 +5,9 @@ class BlogHome extends Component {
     render() {
         return (
             <div className="BlogHome container">
-                <p className="bg-secondary rounded">Welcome to Microblog, our innovative side for communicating information superhighway</p>
+                <p className="bg-secondary rounded">
+                    Welcome to Microblog, our innovative side for communicating information superhighway
+                </p>
                 <TitleList titleList={this.props.titleList} />
             </div>
         );
