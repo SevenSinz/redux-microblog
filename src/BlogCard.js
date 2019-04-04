5f03fcb5e15e7a71cdd30b2cb5bc0c3ad2eb0cca
Stéphane Dryefus <floatingrock@gmail.com>
2019-04-04 08:38:04 -0700

    Small css add.

--- src/BlogCard.js
+++ src/BlogCard.js
@@ -6,9 +6,9 @@ class BlogCard extends Component {
   render() {
       const { id, title, description } = this.props;
     return (
-        <li> 
-        <Link to={`/${id}`}> {title} </Link>  
-        <p>{description}</p>
+        <li className="container border-primary rounded"> 
+          <Link to={`/${id}`}> {title} </Link>  
+          <p>{description}</p>
         </li>
     )
   }
